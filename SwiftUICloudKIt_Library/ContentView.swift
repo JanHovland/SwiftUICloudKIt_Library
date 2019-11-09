25f8e772ff20389793845c1e5b201f8b4f4eefeb
Jan Hovland <jan.hovland@lyse-net>
2019-11-09 17:26:25 +0100

    Initial Commit

---
+++ SwiftUICloudKIt_Library/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICloudKIt_Library
+//
+//  Created by Jan Hovland on 09/11/2019.
+//  Copyright © 2019 Jan Hovland. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
